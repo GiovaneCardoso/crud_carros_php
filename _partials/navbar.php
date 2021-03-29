665afc6d05de879ae440b019c48745713a72f69a
GiovaneCardoso <giovane.cardoso@corebiz.ag>
2021-03-29 19:31:20 -0300

    url change

--- _partials/navbar.php
+++ _partials/navbar.php
@@ -1,16 +1,19 @@
+<?php
+    $baseUrl = "localhost/crud_final/";
+?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">Automoveis</a>
+        <a class="navbar-brand" href="/crud_final">Automoveis</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="<?="/montadora" ?>" >Montadoras</a>
+                    <a class="nav-link" href="/crud_final/montadora">Montadoras</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?="/carros" ?>">Carros</a>
+                    <a class="nav-link" href="/crud_final/carros">Carros</a>
                 </li>
             </ul>
         </div>
